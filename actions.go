fff7ddc4f8707f981db7c96ffe27a3a412f617b7
ken5scal <kengoscal@gmail.com>
2018-07-08 09:52:42 +0900

    recator network connection

--- actions.go
+++ actions.go
@@ -1,107 +1,95 @@
 package main
 
-type DnsRequestAction struct {
-	Action      struct{
-		ActionType      string `json:"actionType"`
-		DnsRequestAction struct{
-			Domain string `json:"domain"`
-			Protocol string  `json:"protocol"`
-			Blocked bool `json:"blocked"`
-		}
-	}
-}
-
-type PortProbeAction struct {
-	Action      struct {
-		ActionType      string `json:"actionType"`
-		PortProbeAction struct {
-			PortProbeDetails []struct {
-				LocalPortDetails struct {
-					Port     int    `json:"port"`
-					PortName string `json:"portName"`
-				} `json:"localPortDetails"`
-				RemoteIPDetails struct {
-					Country struct {
-						CountryName string `json:"countryName"`
-					} `json:"country"`
-					City struct {
-						CityName string `json:"cityName"`
-					} `json:"city"`
-					GeoLocation struct {
-						Lon float64 `json:"lon"`
-						Lat float64 `json:"lat"`
-					} `json:"geoLocation"`
-					Organization struct {
-						AsnOrg string `json:"asnOrg"`
-						Org    string `json:"org"`
-						Isp    string `json:"isp"`
-						Asn    string `json:"asn"`
-					} `json:"organization"`
-					IPAddressV4 string `json:"ipAddressV4"`
-				} `json:"remoteIpDetails"`
-			} `json:"portProbeDetails"`
-			Blocked bool `json:"blocked"`
-		} `json:"portProbeAction"`
-	} `json:"action"`
+type Service struct {
+	ServiceName    string `json:"serviceName"`
+	DetectorID     string `json:"detectorId"`
+	Action         interface{} `json:"action"`
+	ResourceRole   string `json:"resourceRole"`
 	AdditionalInfo struct {
 		ThreatListName  string `json:"threatListName"`
 		ThreatName         int    `json:"threatName"`
 	} `json:"additionalInfo"`
-	Archived       bool   `json:"archived"`
-	Count          int    `json:"count"`
-	DetectorID     string `json:"detectorId"`
 	EventFirstSeen string `json:"eventFirstSeen"`
 	EventLastSeen  string `json:"eventLastSeen"`
-	ResourceRole   string `json:"resourceRole"`
-	ServiceName    string `json:"serviceName"`
+	Archived       bool   `json:"archived"`
+	Count          int    `json:"count"`
 }
 
-type NetworkConnectionAction struct {
-	Action struct {
-		ActionType              string `json:"actionType"`
-		NetworkConnectionAction struct {
-			Blocked             bool   `json:"blocked"`
-			ConnectionDirection string `json:"connectionDirection"`
-			LocalPortDetails    struct {
+type PortProbeAction struct {
+	ActionType      string `json:"actionType"`
+	PortProbeAction struct {
+		PortProbeDetails []struct {
+			LocalPortDetails struct {
 				Port     int    `json:"port"`
 				PortName string `json:"portName"`
 			} `json:"localPortDetails"`
-			Protocol        string `json:"protocol"`
 			RemoteIPDetails struct {
-				City struct {
-					CityName string `json:"cityName"`
-				} `json:"city"`
 				Country struct {
 					CountryName string `json:"countryName"`
 				} `json:"country"`
+				City struct {
+					CityName string `json:"cityName"`
+				} `json:"city"`
 				GeoLocation struct {
-					Lat int `json:"lat"`
-					Lon int `json:"lon"`
+					Lon float64 `json:"lon"`
+					Lat float64 `json:"lat"`
 				} `json:"geoLocation"`
-				IPAddressV4 string `json:"ipAddressV4"`
-
 				Organization struct {
-					Asn int    `json:"asn"`
-					Isp string `json:"isp"`
-					Org string `json:"org"`
+					AsnOrg string `json:"asnOrg"`
+					Org    string `json:"org"`
+					Isp    string `json:"isp"`
+					Asn    string `json:"asn"`
 				} `json:"organization"`
+				IPAddressV4 string `json:"ipAddressV4"`
 			} `json:"remoteIpDetails"`
-			RemotePortDetails struct {
-				Port     int    `json:"port"`
-				PortName string `json:"portName"`
-			} `json:"remotePortDetails"`
-		} `json:"networkConnectionAction"`
-	} `json:"action"`
-	AdditionalInfo struct {
-		ThreatListName  string `json:"threatListName"`
-		Unusual         int    `json:"unusual"`
-		UnusualProtocol string `json:"unusualProtocol"`
-	} `json:"additionalInfo"`
-	Archived       bool   `json:"archived"`
-	Count          int    `json:"count"`
-	DetectorID     string `json:"detectorId"`
-	EventFirstSeen string `json:"eventFirstSeen"`
-	EventLastSeen  string `json:"eventLastSeen"`
-	ResourceRole   string `json:"resourceRole"`
-	ServiceName    string `json:"serviceName"`
+		} `json:"portProbeDetails"`
+		Blocked bool `json:"blocked"`
+	} `json:"dnsRequestAction"`
+}
+
+type DnsRequestAction struct {
+	ActionType      string `json:"actionType"`
+	DnsRequestAction struct{
+		Domain string `json:"domain"`
+		Protocol string  `json:"protocol"`
+		Blocked bool `json:"blocked"`
+	} `json:"networkConnectionAction"`
+}
+
+type NetworkConnectionAction struct {
+	ActionType              string `json:"actionType"`
+	NetworkConnectionAction struct {
+		ConnectionDirection string `json:"connectionDirection"`
+		RemoteIPDetails struct {
+			IPAddressV4 string `json:"ipAddressV4"`
+			Organization struct {
+				Asn int    `json:"asn"`
+				AsnOrg int    `json:"asnOrg"`
+				Isp string `json:"isp"`
+				Org string `json:"org"`
+			} `json:"organization"`
+			Country struct {
+				CountryName string `json:"countryName"`
+				CountryCode string `json:"countryCode"`
+			} `json:"country"`
+			City struct {
+				CityName string `json:"cityName"`
+			} `json:"city"`
+			GeoLocation struct {
+				Lat int `json:"lat"`
+				Lon int `json:"lon"`
+			} `json:"geoLocation"`
+
+		} `json:"remoteIpDetails"`
+		RemotePortDetails struct {
+			Port     int    `json:"port"`
+			PortName string `json:"portName"`
+		} `json:"remotePortDetails"`
+		LocalPortDetails    struct {
+			Port     int    `json:"port"`
+			PortName string `json:"portName"`
+		} `json:"localPortDetails"`
+		Protocol        string `json:"protocol"`
+		Blocked             bool   `json:"blocked"`
+	} `json:"networkConnectionAction"`
 }
